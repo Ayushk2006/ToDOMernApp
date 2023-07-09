c91ec542643da6cbd9e1acec3a5fa4137952b1e0
Ayushk2006 <90329143+Ayushk2006@users.noreply.github.com>
2023-07-09 18:49:23 +0530

    Add files via upload

---
+++ server/database/db.js
@@ -0,0 +1,20 @@
+ 
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
+const Connection = ( ) => {
+     
+     
+    mongoose.connect(process.env.CONNECTION_URL , (err)=>{
+       if(err) 
+       {
+        console.log('unable to connect to the server:');
+        console.log(err);
+       }
+       else
+       console.log('MangoDB connected');
+    })
+    
+}
+
+export default Connection;
